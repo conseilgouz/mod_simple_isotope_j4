c1e84573de5ef44597eaf6e20c0a77a19463f7b3
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-07-02 15:34:27 +0200

    4.6.0
    Joomla 6 : JPATH_PLATFORM deprecated

--- src/Field/SqlnoerrField.php
+++ src/Field/SqlnoerrField.php
@@ -9,7 +9,7 @@
 
 namespace ConseilGouz\Module\SimpleIsotope\Site\Field;
 
-defined('JPATH_PLATFORM') or die;
+defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Field\SqlField;
 use Joomla\Database\DatabaseInterface;
